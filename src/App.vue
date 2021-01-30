bf365daca724c3ebadf71dcbe62832f328d7448b
luluxiaocc <516987327@qq.com>
2021-01-30 19:34:51 +0800

    基础项目

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ccMap3d></ccMap3d>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ccMap3d from "./components/ccMap3d.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    ccMap3d,
+  },
+};
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/ccMap3d.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+    <p>基础结构</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ccMap3d",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+</style>
